289c85f0d18a6501e582a2a0f0cbd7b5ce413775
dahyun <ekenfl93@gmail.com>
2024-01-21 08:49:19 +0900

    Appy details forms

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -16,15 +16,31 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
 
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
-
+import Button from '@mui/material/Button';
+import HomeIcon from '@mui/icons-material/Home';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemButton from '@mui/material/ListItemButton';
+import CommentIcon from '@mui/icons-material/Comment';
+import IconButton from '@mui/material/IconButton';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
 import { useDispatch, useSelector } from 'react-redux';
+import { WindowSharp } from '@mui/icons-material';
+
+import Table1 from '../components/Table';
+import Dialog from '../components/Dialog';
 
 function Details() {
   const [age, setAge] = React.useState('');
@@ -40,9 +56,14 @@ function Details() {
   // };
 
   const lists = useSelector((state) => state.datas);
-  const json_data = lists[item];
+  const json_data = lists.find(e => e.id === Number(item));
 
   return (
+    // <>
+    //   <Table1></Table1>
+    //   <Table1></Table1>
+    //   <Table1></Table1>
+    // </>
     <>
     <Grid container spacing={3}>
       <Grid item xs></Grid>
@@ -51,51 +72,191 @@ function Details() {
           {json_data["title"]}
         </Typography>
         
-        <Accordion defaultExpanded>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1-content"
-            id="panel1-header"
-          >
-            <Typography>세대수</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            {/* <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+        
+        <List sx={{ width: '100%', maxWidth: 500 }}>
+          <ListItem
+            key={8}
+            disableGutters
+            secondaryAction={
               <OutlinedInput
-                id="outlined-adornment-weight"
-                endAdornment={<InputAdornment position="end">세대</InputAdornment>}
-                aria-describedby="outlined-weight-helper-text"
-                inputProps={{
-                  'aria-label': 'weight',
-                }}
-                placeholder="ex) 500"
-              />
-            </FormControl> */}
-            <FormControl variant="standard" sx={{ m: 1, mt: 3, width: '25ch' }}>
-              <Input
-                id="standard-adornment-weight"
-                endAdornment={<InputAdornment position="end">세대</InputAdornment>}
-                aria-describedby="standard-weight-helper-text"
-                inputProps={{
-                  'aria-label': 'weight',
-                }}
-                placeholder="ex) 500"
-              />
-            </FormControl>
-          </AccordionDetails>
-        </Accordion>
-
-        <Accordion defaultExpanded>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1-content"
-            id="panel1-header"
+                inputProps={{ style: { textAlign: "right" } }}
+                style={{ borderRadius: "20px", width: "200px" }}
+                size='small'
+                endAdornment={<InputAdornment position="end">원</InputAdornment>}
+              />              
+              }
           >
-            <Typography>관리</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <FormControl>
-            <RadioGroup
+            <ListItemText primary={`예상 매매가`} />
+          </ListItem>
+          <ListItem
+            key={9}
+            disableGutters
+            secondaryAction={
+              <OutlinedInput
+                inputProps={{ style: { textAlign: "right" } }}
+                style={{ borderRadius: "20px", width: "200px" }}
+                size='small'
+                startAdornment={<InputAdornment position="start">82%</InputAdornment>}
+                endAdornment={<InputAdornment position="end">원</InputAdornment>}
+              />              
+              }
+          >
+            <ListItemText primary={`예상 전세가`} />
+          </ListItem>
+          <ListItem
+            key={0}
+            disableGutters
+            secondaryAction={
+              <OutlinedInput
+                style={{ borderRadius: "20px" }}
+                size='small'
+              />              
+              }
+          >
+            <ListItemText primary={`주소`} />
+          </ListItem>
+          <ListItem
+            key={10}
+            disableGutters
+            secondaryAction={
+              <OutlinedInput
+                inputProps={{ style: { textAlign: "right" } }}
+                style={{ borderRadius: "20px", width: "200px" }}
+                size='small'
+                startAdornment={<InputAdornment position="start">00년차</InputAdornment>}
+                endAdornment={<InputAdornment position="end">년</InputAdornment>}
+              />              
+              }
+          >
+            <ListItemText primary={`시공년도`} />
+          </ListItem>
+         <ListItem
+            key={1}
+            disableGutters
+            secondaryAction={
+              <OutlinedInput
+                inputProps={{ style: { textAlign: "right" } }}
+                style={{ borderRadius: "20px", width: "200px" }}
+                size='small'
+                endAdornment={<InputAdornment position="end">세대</InputAdornment>}
+              />              
+              }
+          >
+            <ListItemText primary={`세대수`} />
+          </ListItem>
+          <ListItem
+            key={11}
+            disableGutters
+            secondaryAction={
+              <OutlinedInput
+                inputProps={{ style: { textAlign: "right" } }}
+                style={{ borderRadius: "20px", width: "200px" }}
+                size='small'
+                startAdornment={<InputAdornment position="start">세대당 1.02대</InputAdornment>}
+                endAdornment={<InputAdornment position="end">대</InputAdornment>}
+              />              
+              }
+          >
+            <ListItemText primary={`주차`} />
+          </ListItem>
+          <ListItem
+            key={3}
+            disableGutters
+            secondaryAction={
+              <OutlinedInput
+                style={{ borderRadius: "20px" }}
+                size='small'
+                startAdornment={<InputAdornment position="start">지하철</InputAdornment>}
+              />      
+            }
+          >
+            <ListItemText primary={`대중교통`} />
+          </ListItem>
+          <ListItem
+            style={{ marginTop: '12px'}}
+            key={4}
+            disableGutters
+            secondaryAction={
+              <OutlinedInput
+                style={{ borderRadius: "20px" }}
+                size='small'
+                startAdornment={<InputAdornment position="start">버스</InputAdornment>}
+              />            
+            }
+          >
+            <ListItemText primary={''} />
+          </ListItem>
+          <br></br>
+          <ListItem
+            key={5}
+            disableGutters
+            secondaryAction={
+              <FormControl component="fieldset">
+                <FormGroup aria-label="position" row>
+                  <FormControlLabel
+                    value="bottom"
+                    control={<Checkbox />}
+                    label="초"
+                    labelPlacement="bottom"
+                  />
+                  <FormControlLabel
+                    value="bottom"
+                    control={<Checkbox />}
+                    label="중"
+                    labelPlacement="bottom"
+                  />
+                  <FormControlLabel
+                    value="bottom"
+                    control={<Checkbox />}
+                    label="고"
+                    labelPlacement="bottom"
+                  />
+                </FormGroup>
+              </FormControl>          
+            }
+          >
+            {/* <ListItemIcon style={{ cursor: 'pointer' }} onClick={()=>console.log('CLICK!!')}>
+              <CommentIcon />
+            </ListItemIcon> */}
+            <ListItemText primary={`학교`} />
+          </ListItem>
+          <br></br>
+          <ListItem
+            key={6}
+            disableGutters
+            secondaryAction={
+              <FormControl component="fieldset" >
+                <FormGroup aria-label="position" row>
+                  <FormControlLabel
+                    value="bottom"
+                    control={<Checkbox />}
+                    label="계단식"
+                    labelPlacement="bottom"
+                  />
+                  <FormControlLabel
+                    value="bottom"
+                    control={<Checkbox />}
+                    label="복도식"
+                    labelPlacement="bottom"
+                  />
+                  <FormControlLabel
+                    value="bottom"
+                    control={<Checkbox />}
+                    label="복합식"
+                    labelPlacement="bottom"
+                  />
+                </FormGroup>
+              </FormControl>        
+            }
+          >
+            <ListItemText primary={`현관구조`} onClick={()=>console.log("rr")}/>
+          </ListItem>
+          <br></br>
+          <ListItem
+            key={7}
+            disableGutters
+            secondaryAction={
+              <RadioGroup
               row
               aria-labelledby="demo-form-control-label-placement"
               name="position"
@@ -104,68 +265,68 @@ function Details() {
               <FormControlLabel
                 value="1"
                 control={<Radio/>}
-                label="1"
+                label="지역"
                 labelPlacement="bottom"
               />
               <FormControlLabel
                 value="2"
                 control={<Radio size="medium"/>}
-                label="2"
+                label="개별"
                 labelPlacement="bottom"
               />
               <FormControlLabel
                 value="3"
                 control={<Radio size="medium"/>}
-                label="3"
+                label="중앙"
                 labelPlacement="bottom"
               />
-              <FormControlLabel
-                value="4"
-                control={<Radio size="medium"/>}
-                label="4"
-                labelPlacement="bottom"
-              />
-              <FormControlLabel
-                value="5"
-                control={<Radio size="medium"/>}
-                label="5"
-                labelPlacement="bottom"
-              />
-            </RadioGroup>
-          </FormControl>
-          </AccordionDetails>
-        </Accordion>
-
-        <Accordion defaultExpanded>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1-content"
-            id="panel1-header"
+            </RadioGroup>           
+              }
           >
-            <Typography>관리</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <FormControl sx={{ minWidth: 120 }}>
-            <Autocomplete
-              disablePortal
-              id="combo-box-demo"
-              options={subwayLines}
-              sx={{ width: 200 }}
-              renderInput={(params) => <TextField {...params} label="Lines" />}
-            />
-            </FormControl>
-            <FormControl sx={{ minWidth: 120 }}>
-            <Autocomplete
-              disabled={true}
-              disablePortal
-              id="combo-box-demo"
-              options={subwayStations}
-              sx={{ width: 200 }}
-              renderInput={(params) => <TextField {...params} label="Stations" />}
-            />
-            </FormControl>
-          </AccordionDetails>
-        </Accordion>
+            <ListItemText primary={`난방`} />
+          </ListItem>
+          <br></br>
+          <ListItem
+            key={2}
+            disableGutters
+            secondaryAction={
+              <RadioGroup
+              row
+              aria-labelledby="demo-form-control-label-placement"
+              name="position"
+              defaultValue="bottom"
+            >
+              <FormControlLabel
+                value="1"
+                control={<Radio/>}
+                label="나쁨"
+                labelPlacement="bottom"
+              />
+              <FormControlLabel
+                value="2"
+                control={<Radio size="medium"/>}
+                label="보통"
+                labelPlacement="bottom"
+              />
+              <FormControlLabel
+                value="3"
+                control={<Radio size="medium"/>}
+                label="좋음"
+                labelPlacement="bottom"
+              />
+            </RadioGroup>           
+              }
+          >
+            <ListItemText primary={`관리상태`} />
+          </ListItem>
+          <br></br>
+        </List>
+        <br></br>
+        <Button fullWidth={true} size="large" style={{ borderRadius: "20px" }} variant="contained" color="success" startIcon={<HomeIcon />} onClick={()=>window.open('https://land.naver.com/')}>
+          네이버 부동산 열기
+        </Button>
+
+        <Dialog></Dialog>
       </Grid>
         <Grid item xs></Grid>
     </Grid>
