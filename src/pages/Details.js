eda3b27868e21a4a7d7f9a0bbf0967af3080d58e
Dahyun Kyung <dahyun93@deepnoid.com>
2024-01-23 08:52:23 +0900

    Link details and firebase DB

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -36,10 +36,12 @@ import CommentIcon from '@mui/icons-material/Comment';
 import IconButton from '@mui/material/IconButton';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
-
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import { useDispatch, useSelector } from 'react-redux';
 import { WindowSharp } from '@mui/icons-material';
-
+import { Link } from 'react-router-dom';
+import AddIcon from '@mui/icons-material/Add';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import { todoUpdate } from '../store';
 
 // import Table1 from '../components/Table';
@@ -56,6 +58,8 @@ function Details() {
   const itemData = lists[item];
   console.log('itemData = ', itemData);
 
+  const nowYear = new Date().getFullYear();
+  const naverLandUrl = 'https://land.naver.com';
   // const [title, setTitle] = React.useState(itemData.title);
   // const [address, setAddress] = React.useState(itemData.info.address);
   // const [year, setYear] = React.useState(itemData.info.year_of_construction);
@@ -69,7 +73,7 @@ function Details() {
   // const [naverBdsUrl, setNaverBdsUrl] = React.useState(itemData.info.naver_bds_url);
 
   // const [jsonData, setJsonData] = React.useState(itemData);
-  const handleBlur = (e, info) => {
+  const updateValue = (e, info) => {
     dispatch(todoUpdate(itemData.id, info));
   };
 
@@ -93,10 +97,15 @@ function Details() {
             disableGutters
             secondaryAction={
               <OutlinedInput
+                defaultValue={itemData.info['expected_price']}
                 inputProps={{ style: { textAlign: "right" } }}
                 style={{ borderRadius: "20px", width: "200px" }}
                 size='small'
                 endAdornment={<InputAdornment position="end">원</InputAdornment>}
+                onBlur={(e)=>{
+                  const info = {...itemData.info, ['expected_price']:e.target.value};
+                  updateValue(e, info);
+                }}
               />              
               }
           >
@@ -107,11 +116,16 @@ function Details() {
             disableGutters
             secondaryAction={
               <OutlinedInput
+                defaultValue={itemData.info['expected_rent_price']}
                 inputProps={{ style: { textAlign: "right" } }}
                 style={{ borderRadius: "20px", width: "200px" }}
                 size='small'
                 startAdornment={<InputAdornment position="start">82%</InputAdornment>}
                 endAdornment={<InputAdornment position="end">원</InputAdornment>}
+                onBlur={(e)=>{
+                  const info = {...itemData.info, ['expected_rent_price']:e.target.value};
+                  updateValue(e, info);
+                }}
               />              
               }
           >
@@ -124,11 +138,11 @@ function Details() {
                 <Tooltip title={itemData.info['address']} placement="top">
                   <OutlinedInput
                     defaultValue={itemData.info['address']}
-                    style={{ borderRadius: "20px" }}
+                    style={{ borderRadius: "20px", width: "250px" }}
                     size='small'
                     onBlur={(e)=>{
                       const info = {...itemData.info, ['address']:e.target.value};
-                      handleBlur(e, info);
+                      updateValue(e, info);
                     }}
                   />              
                 </Tooltip>
@@ -141,11 +155,16 @@ function Details() {
             disableGutters
             secondaryAction={
               <OutlinedInput
+                defaultValue={itemData.info['year_of_construction']}
                 inputProps={{ style: { textAlign: "right" } }}
                 style={{ borderRadius: "20px", width: "200px" }}
                 size='small'
-                startAdornment={<InputAdornment position="start">00년차</InputAdornment>}
+                startAdornment={<InputAdornment position="start">{itemData.info['year_of_construction'] === '' ? 0 : (nowYear-itemData.info['year_of_construction'])+1}년차</InputAdornment>}
                 endAdornment={<InputAdornment position="end">년</InputAdornment>}
+                onBlur={(e)=>{
+                  const info = {...itemData.info, ['year_of_construction']:e.target.value};
+                  updateValue(e, info);
+                }}
               />
               }
           >
@@ -163,7 +182,7 @@ function Details() {
                 endAdornment={<InputAdornment position="end">세대</InputAdornment>}
                 onBlur={(e)=>{
                   const info = {...itemData.info, ['number_of_households']:e.target.value};
-                  handleBlur(e, info);
+                  updateValue(e, info);
                 }}
               />              
               }
@@ -183,7 +202,7 @@ function Details() {
                 endAdornment={<InputAdornment position="end">대</InputAdornment>}
                 onBlur={(e)=>{
                   const info = {...itemData.info, ['parking']:e.target.value};
-                  handleBlur(e, info);
+                  updateValue(e, info);
                 }}
               />              
               }
@@ -195,9 +214,21 @@ function Details() {
             disableGutters
             secondaryAction={
               <OutlinedInput
-                style={{ borderRadius: "20px" }}
+                defaultValue={itemData.info['subway']}
+                inputProps={{ style: { textAlign: "right" } }}
+                style={{ borderRadius: "20px", width: "250px" }}
                 size='small'
+                placeholder='4호선 사당역 도보 5분'
                 startAdornment={<InputAdornment position="start">지하철</InputAdornment>}
+                endAdornment={
+                  <Tooltip title='지하철 추가' placement="top">
+                    <AddCircleOutlineIcon style={{ cursor: "pointer" }} onClick={()=>window.open('https://land.naver.com/')}></AddCircleOutlineIcon>
+                  </Tooltip>  
+                  }
+                onBlur={(e)=>{
+                  const info = {...itemData.info, ['subway']:e.target.value};
+                  updateValue(e, info);
+                }}
               />      
             }
           >
@@ -209,11 +240,18 @@ function Details() {
             disableGutters
             secondaryAction={
               <OutlinedInput
-                style={{ borderRadius: "20px" }}
+                defaultValue={itemData.info['bus']}
+                inputProps={{ style: { textAlign: "right" } }}
+                style={{ borderRadius: "20px", width: "250px" }}
                 size='small'
+                placeholder='도보 5분'
                 startAdornment={<InputAdornment position="start">버스</InputAdornment>}
               />            
             }
+            onBlur={(e)=>{
+              const info = {...itemData.info, ['bus']:e.target.value};
+              updateValue(e, info);
+            }}
           >
             <ListItemText primary={''} />
           </ListItem>
@@ -222,28 +260,33 @@ function Details() {
             key={5}
             disableGutters
             secondaryAction={
-              <FormControl component="fieldset">
-                <FormGroup aria-label="position" row>
-                  <FormControlLabel
-                    value="bottom"
-                    control={<Checkbox />}
-                    label="초"
-                    labelPlacement="bottom"
-                  />
-                  <FormControlLabel
-                    value="bottom"
-                    control={<Checkbox />}
-                    label="중"
-                    labelPlacement="bottom"
-                  />
-                  <FormControlLabel
-                    value="bottom"
-                    control={<Checkbox />}
-                    label="고"
-                    labelPlacement="bottom"
-                  />
-                </FormGroup>
-              </FormControl>          
+              <FormGroup aria-label="position" row 
+                onClick={(e)=>{
+                  let result;
+                  e.target.checked === true ? result = itemData.info['school'] + e.target.value : result = itemData.info['school'].replaceAll(e.target.value, '');
+                  const info = {...itemData.info, ['school']:result};
+                  updateValue(e, info);
+                }
+              }>
+                <FormControlLabel
+                  value="0"
+                  control={<Checkbox defaultChecked={itemData.info['school'].includes('0') ? true : false}/>}
+                  label="초등"
+                  labelPlacement="bottom"
+                />
+                <FormControlLabel
+                  value="1"
+                  control={<Checkbox defaultChecked={itemData.info['school'].includes('1') ? true : false}/>}
+                  label="중"
+                  labelPlacement="bottom"
+                />
+                <FormControlLabel
+                  value="2"
+                  control={<Checkbox defaultChecked={itemData.info['school'].includes('2') ? true : false}/>}
+                  label="고등"
+                  labelPlacement="bottom"
+                />
+              </FormGroup>
             }
           >
             {/* <ListItemIcon style={{ cursor: 'pointer' }} onClick={()=>console.log('CLICK!!')}>
@@ -256,28 +299,33 @@ function Details() {
             key={6}
             disableGutters
             secondaryAction={
-              <FormControl component="fieldset" >
-                <FormGroup aria-label="position" row>
-                  <FormControlLabel
-                    value="bottom"
-                    control={<Checkbox />}
-                    label="계단식"
-                    labelPlacement="bottom"
+              <FormGroup aria-label="position" row 
+                onClick={(e)=>{
+                  let result;
+                  e.target.checked === true ? result = itemData.info['entrance_structure'] + e.target.value : result = itemData.info['entrance_structure'].replaceAll(e.target.value, '');
+                  const info = {...itemData.info, ['entrance_structure']:result};
+                  updateValue(e, info);
+                }
+              }>
+                <FormControlLabel
+                  value="0"
+                  control={<Checkbox defaultChecked={itemData.info['entrance_structure'].includes('0') ? true : false}/>}
+                  label="계단"
+                  labelPlacement="bottom"
+                />
+                <FormControlLabel
+                  value="1"
+                  control={<Checkbox defaultChecked={itemData.info['entrance_structure'].includes('1') ? true : false}/>}
+                  label="복도"
+                  labelPlacement="bottom"
                   />
-                  <FormControlLabel
-                    value="bottom"
-                    control={<Checkbox />}
-                    label="복도식"
-                    labelPlacement="bottom"
-                  />
-                  <FormControlLabel
-                    value="bottom"
-                    control={<Checkbox />}
-                    label="복합식"
-                    labelPlacement="bottom"
-                  />
-                </FormGroup>
-              </FormControl>        
+                <FormControlLabel
+                  value="2"
+                  control={<Checkbox defaultChecked={itemData.info['entrance_structure'].includes('2') ? true : false}/>}
+                  label="복합"
+                  labelPlacement="bottom"
+                />
+              </FormGroup>
             }
           >
             <ListItemText primary={`현관구조`} onClick={()=>console.log("rr")}/>
@@ -288,30 +336,34 @@ function Details() {
             disableGutters
             secondaryAction={
               <RadioGroup
-              row
-              aria-labelledby="demo-form-control-label-placement"
-              name="position"
-              defaultValue="bottom"
-            >
-              <FormControlLabel
-                value="1"
-                control={<Radio/>}
-                label="지역"
-                labelPlacement="bottom"
-              />
-              <FormControlLabel
-                value="2"
-                control={<Radio size="medium"/>}
-                label="개별"
-                labelPlacement="bottom"
-              />
-              <FormControlLabel
-                value="3"
-                control={<Radio size="medium"/>}
-                label="중앙"
-                labelPlacement="bottom"
-              />
-            </RadioGroup>           
+                row
+                aria-labelledby="demo-form-control-label-placement"
+                name="position"
+                defaultValue={itemData.info['heating']}
+                onClick={(e)=>{
+                  const info = {...itemData.info, ['heating']:e.target.value};
+                  updateValue(e, info);
+                }}
+              >
+                <FormControlLabel
+                  value="0"
+                  control={<Radio/>}
+                  label="지역"
+                  labelPlacement="bottom"
+                />
+                <FormControlLabel
+                  value="1"
+                  control={<Radio size="medium"/>}
+                  label="개별"
+                  labelPlacement="bottom"
+                />
+                <FormControlLabel
+                  value="2"
+                  control={<Radio size="medium"/>}
+                  label="중앙"
+                  labelPlacement="bottom"
+                />
+              </RadioGroup>           
               }
           >
             <ListItemText primary={`난방`} />
@@ -322,41 +374,72 @@ function Details() {
             disableGutters
             secondaryAction={
               <RadioGroup
-              row
-              aria-labelledby="demo-form-control-label-placement"
-              name="position"
-              defaultValue="bottom"
-            >
-              <FormControlLabel
-                value="1"
-                control={<Radio/>}
-                label="나쁨"
-                labelPlacement="bottom"
-              />
-              <FormControlLabel
-                value="2"
-                control={<Radio size="medium"/>}
-                label="보통"
-                labelPlacement="bottom"
-              />
-              <FormControlLabel
-                value="3"
-                control={<Radio size="medium"/>}
-                label="좋음"
-                labelPlacement="bottom"
-              />
-            </RadioGroup>           
+                row
+                aria-labelledby="demo-form-control-label-placement"
+                name="position"
+                defaultValue={itemData.info['management_status']}
+                onClick={(e)=>{
+                  const info = {...itemData.info, ['management_status']:e.target.value};
+                  updateValue(e, info);
+                }}
+              >
+                <FormControlLabel
+                  value="0"
+                  control={<Radio/>}
+                  label="나쁨"
+                  labelPlacement="bottom"
+                />
+                <FormControlLabel
+                  value="1"
+                  control={<Radio size="medium"/>}
+                  label="보통"
+                  labelPlacement="bottom"
+                />
+                <FormControlLabel
+                  value="2"
+                  control={<Radio size="medium"/>}
+                  label="좋음"
+                  labelPlacement="bottom"
+                />
+              </RadioGroup>           
               }
           >
             <ListItemText primary={`관리상태`} />
           </ListItem>
           <br></br>
+          <ListItem
+            key={81}
+            disableGutters
+            secondaryAction={
+                <OutlinedInput
+                  defaultValue={itemData.info['naver_bds_url']}
+                  placeholder={naverLandUrl}
+                  inputProps={{ style: { textAlign: "left" } }}
+                  style={{ borderRadius: "20px", width: "250px" }}
+                  size='small'
+                  endAdornment={
+                    <Tooltip title='네이버 부동산으로 이동' placement="top">
+                      <ExitToAppIcon style={{ cursor: "pointer" }} color="success" onClick={()=>window.open(itemData.info['naver_bds_url'])}></ExitToAppIcon>
+                    </Tooltip>  
+                    }
+                  onBlur={(e)=>{
+                    let result;
+                    e.target.value === '' ? result = naverLandUrl : e.target.value.includes('land.naver.com') ? result = e.target.value : result = naverLandUrl;
+                    const info = {...itemData.info, ['naver_bds_url']:result};
+                    updateValue(e, info);
+                  }}
+                />
+              }
+          >
+            <ListItemText primary={`네이버 부동산 URL`} />
+          </ListItem>
+          <br></br>
+          <Link to='/' style={{ textDecoration: "none", color: "inherit" }}>
+            <Button fullWidth={true} size="large" style={{ borderRadius: "20px" }} variant="outlined" >
+              뒤로가기
+            </Button>
+          </Link>
         </List>
-        <br></br>
-        <Button fullWidth={true} size="large" style={{ borderRadius: "20px" }} variant="contained" color="success" startIcon={<HomeIcon />} onClick={()=>window.open('https://land.naver.com/')}>
-          네이버 부동산 열기
-        </Button>
-
       </Grid>
         <Grid item xs></Grid>
     </Grid>
