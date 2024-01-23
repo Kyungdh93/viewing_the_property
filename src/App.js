5269dffb384efeb9bb1a4a6797f64dc91d7dcf00
dahyun <ekenfl93@gmail.com>
2024-01-23 22:51:44 +0900

    Add isMobile

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import { login, getAllData } from './store';
 import { useDispatch, useSelector } from 'react-redux';
 import { ref, child, get } from "firebase/database";
 import { db } from "./firebase-config";
+import { isBrowser, isMobile } from 'react-device-detect';
 
 function App() {
   // const [userData, setUserData] = useState(null);
@@ -20,29 +21,46 @@ function App() {
   const dispatch = useDispatch();
 
   function handleGoogleLogin() {
-    const provider = new GoogleAuthProvider(); // provider 구글 설정
-    signInWithPopup(auth, provider) // 팝업창 띄워서 로그인
-      .then((data) => {
-        // setUserData(data.user); // user data 설정
-        dispatch(login(data.user));
-        console.log(data); // console에 UserCredentialImpl 출력
-        const dbRef = ref(db);
-          get(child(dbRef, "/datas"))
-            .then(snapshot => {
-            if (snapshot.exists()) {
-              dispatch(getAllData(snapshot.val()));
-              console.log(snapshot.val());
-            } else {
-              console.log("No data available");
-            }
-          })
-            .catch(error => {
-            console.error(error);
-          });
+    if (isMobile) {
+      dispatch(login('dahyun'));
+      const dbRef = ref(db);
+      get(child(dbRef, "/datas"))
+        .then(snapshot => {
+        if (snapshot.exists()) {
+          dispatch(getAllData(snapshot.val()));
+          console.log(snapshot.val());
+        } else {
+          console.log("No data available");
+        }
       })
-      .catch((err) => {
-        console.log(err);
+      .catch(error => {
+        console.error(error);
       });
+    } else {
+      const provider = new GoogleAuthProvider(); // provider 구글 설정
+      signInWithPopup(auth, provider) // 팝업창 띄워서 로그인
+        .then((data) => {
+          dispatch(login(data.user));
+          console.log(data); // console에 UserCredentialImpl 출력
+          const dbRef = ref(db);
+            get(child(dbRef, "/datas"))
+              .then(snapshot => {
+              if (snapshot.exists()) {
+                dispatch(getAllData(snapshot.val()));
+                console.log(snapshot.val());
+              } else {
+                console.log("No data available");
+              }
+            })
+              .catch(error => {
+              console.error(error);
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
   }
 
   return (
