e07a49d75bf0f19a961dd5d9c26891bdb8cc24df
dahyun <ekenfl93@gmail.com>
2024-01-27 17:41:43 +0900

    Updating Table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
+// import { styled } from '@mui/material/styles';
+import { styled } from "styled-components";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
@@ -12,35 +13,84 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from "react-router-dom";
 import { todoUpdate } from '../store';
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  width: '150px', 
-  height: '40px',
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
+const MyTableHeader = styled(TableCell)(
+  ({ theme }) => ({
+    backgroundColor: theme.colors.colorMain, 
+    color: theme.colors.colorMainFont,
+    width: "50px", 
+    height: "40px",
+    textAlign: "center",
+  })
+)
 
-export default function CustomizedTables() {
+const MyTableCell = styled(TableCell)(
+  ({ theme }) => ({
+    backgroundColor: theme.colors.colorDarkGray, 
+    width: "150px", 
+    height: "40px",
+    textAlign: "right",
+  })
+)
+
+const MyCheckbox = styled(Checkbox)(
+  ({ theme }) => ({
+    color: theme.colors.colorMainFont,
+    '&.Mui-checked': {
+      color: theme.colors.colorDiRed
+    }
+  })
+);
+
+const MyRadio = styled(Radio)(
+  ({ theme }) => ({
+    color: theme.colors.colorMainFont,
+    '&.Mui-checked': {
+      color: theme.colors.colorDiRed
+    }
+  })
+);
+
+// const StyledTableCell = styled(TableCell)(({ theme }) => ({
+//   width: '150px', 
+//   height: '40px',
+//   [`&.${tableCellClasses.head}`]: {
+//     backgroundColor: theme.palette.common.black,
+//     color: theme.palette.common.white,
+//   },
+//   [`&.${tableCellClasses.body}`]: {
+//     fontSize: 14,
+//   },
+// }));
+
+// const StyledTableRow = styled(TableRow)(({ theme }) => ({
+//   '&:nth-of-type(odd)': {
+//     backgroundColor: theme.palette.action.hover,
+//   },
+//   // hide last border
+//   '&:last-child td, &:last-child th': {
+//     border: 0,
+//   },
+// }));
+
+export default function CustomizedTables(props) {
   const dispatch = useDispatch();
-
   const { item } = useParams();
   const lists = useSelector((state) => state.datas);
   const itemData = lists[item];
-  console.log('itemData in Table.js = ', itemData);
+  // console.log('itemData in Table.js = ', itemData);
+
+  const [expectedPrice, setExpectedPrice] = React.useState(Number(itemData.info['expected_price']));
+  const [expectedRentPrice, setExpectedRentPrice] = React.useState(Number(itemData.info['expected_rent_price']));
+  const mappingData = props.mappingData;
+  const orderArray = props.orderArray;
+  const nowYear = props.nowYear;
+  const naverLandUrl = props.naverLandUrl;
 
   const [editable, setEditable] = React.useState('');
 
@@ -55,44 +105,108 @@ export default function CustomizedTables() {
   };
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        {/* <TableHead>
-          <TableRow>
-            <StyledTableCell>주소</StyledTableCell>
-            <StyledTableCell>시공년도</StyledTableCell>
-            <StyledTableCell>세대수</StyledTableCell>
-            <StyledTableCell>대중교통</StyledTableCell>
-            <StyledTableCell>학교</StyledTableCell>
-          </TableRow>
-        </TableHead> */}
-        <TableBody>
-          {Object.keys(itemData.info).map((keyName, index) => (
-            <StyledTableRow key={keyName}>
-              <StyledTableCell sx={{ backgroundColor: "green" }}> {keyName} </StyledTableCell>
-              <StyledTableCell onClick={()=>{testClick(keyName)}}>
-                {
-                  editable === keyName ? (
-                    <TextField 
-                      defaultValue={itemData.info[keyName]} 
-                      autoFocus 
-                      size='small' 
-                      sx={{width: '150px', height: '40px' }}
-                      onBlur={(e)=>{
+      <Table sx={{ minWidth: 100, width: 500, margin: "auto" }} aria-label="customized table">
+        <TableBody sx={{tableLayout: "fixed"}}>
+          {orderArray.map((keyName, index) => (
+            <TableRow key={keyName}>
+              {/* {
+                index % 2 === 0 ? (
+                  <StyledTableCell rowSpan={2} sx={{ backgroundColor: "blue", width: "50px" }}> {mappingData[keyName]} </StyledTableCell>
+                  ) : (
+                  <></>
+                )
+              } */}
+              <MyTableHeader> {mappingData[keyName][0]} </MyTableHeader>
+              {
+                mappingData[keyName][1] === 'input' ? (
+                  <MyTableCell onClick={()=>{testClick(keyName)}}>
+                    {
+                      editable === keyName ? (
+                        <TextField 
+                          defaultValue={itemData.info[keyName]} 
+                          autoFocus 
+                          size='small' 
+                          sx={{width: '150px', height: '40px' }}
+                          onBlur={(e)=>{
+                            const info = {...itemData.info, [keyName]:e.target.value};
+                            setEditable('');
+                            updateValue(e, info);
+                          }}
+                        />
+                        ) : (
+                        itemData.info[keyName]
+                      )
+                    }
+                  </MyTableCell>
+                ) : mappingData[keyName][1] === 'checkbox' ? (
+                  <MyTableCell>
+                    <FormGroup aria-label="position" row
+                      onClick={(e)=>{
+                        let result;
+                        e.target.checked === true ? result = itemData.info[keyName] + e.target.value : result = itemData.info[keyName].replaceAll(e.target.value, '');
+                        const info = {...itemData.info, [keyName]:result};
+                        updateValue(e, info);
+                      }
+                    }>
+                      <FormControlLabel
+                        value="0"
+                        control={<MyCheckbox defaultChecked={itemData.info[keyName].includes('0') ? true : false}/>}
+                        label="초등"
+                        labelPlacement="bottom"
+                      />
+                      <FormControlLabel
+                        value="1"
+                        control={<MyCheckbox defaultChecked={itemData.info[keyName].includes('1') ? true : false}/>}
+                        label="중"
+                        labelPlacement="bottom"
+                      />
+                      <FormControlLabel
+                        value="2"
+                        control={<MyCheckbox defaultChecked={itemData.info[keyName].includes('2') ? true : false}/>}
+                        label="고등"
+                        labelPlacement="bottom"
+                      />
+                    </FormGroup>
+                  </MyTableCell>
+                  ) : mappingData[keyName][1] === 'radio' ? (
+                  <MyTableCell>
+                    <RadioGroup
+                      row
+                      aria-labelledby="demo-form-control-label-placement"
+                      name="position"
+                      defaultValue={itemData.info[keyName]}
+                      onClick={(e)=>{
                         const info = {...itemData.info, [keyName]:e.target.value};
-                        setEditable('');
                         updateValue(e, info);
                       }}
-                    />
-                    ) : (
-                    itemData.info[keyName]
-                  )
-                }
-              </StyledTableCell>
-            </StyledTableRow>
+                    >
+                      <FormControlLabel
+                        value="0"
+                        control={<MyRadio/>}
+                        label={keyName === 'entrance_structure' ? "계단식" : keyName === "heating" ? "지역" : "나쁨"}
+                        labelPlacement="bottom"
+                      />
+                      <FormControlLabel
+                        value="1"
+                        control={<MyRadio size="medium"/>}
+                        label={keyName === 'entrance_structure' ? "복도식" : keyName === "heating" ? "개별" : "보통"}
+                        labelPlacement="bottom"
+                      />
+                      <FormControlLabel
+                        value="2"
+                        control={<MyRadio size="medium"/>}
+                        label={keyName === 'entrance_structure' ? "복합식" : keyName === "heating" ? "중앙" : "좋음"}
+                        labelPlacement="bottom"
+                      />
+                    </RadioGroup>
+                  </MyTableCell>
+                  ) : (
+                  <MyTableCell>{itemData.info[keyName]}</MyTableCell>
+                )
+              }
+              </TableRow>
           ))}
         </TableBody>
       </Table>
-    </TableContainer>
   );
 }
