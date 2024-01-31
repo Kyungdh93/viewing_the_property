4abc9508cb14676b23364121f47bc6ce671e1d91
Dahyun Kyung <dahyun93@deepnoid.com>
2024-02-01 08:50:57 +0900

    Add local login

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,11 +2,11 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import GoogleIcon from '@mui/icons-material/Google';
+import AccessibilityIcon from '@mui/icons-material/Accessibility';
 import Divider from '@mui/material/Divider';
 
 import { auth } from "../firebase-config";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-
 import { login } from '../store';
 import { useDispatch } from 'react-redux';
 import { isMobile } from 'react-device-detect';
@@ -39,12 +39,8 @@ const Auth = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   
-  function handleGoogleLogin() {
-    if (isMobile) {
-      dispatch(login('dahyun'));
-      navigate('/');
-    } else {
-      const provider = new GoogleAuthProvider(); // provider 구글 설정
+  const handleGoogleLogin = () => {
+    const provider = new GoogleAuthProvider(); // provider 구글 설정
       signInWithPopup(auth, provider) // 팝업창 띄워서 로그인
         .then((data) => {
           dispatch(login(data.user));
@@ -54,13 +50,18 @@ const Auth = () => {
         .catch((err) => {
           console.log(err);
         });
-    };
+  };
+
+  const handleLocalLogin = () => {
+    dispatch(login('local'));
+    navigate('/');
   }
 
   return (
-    <>
-      <MyButton startIcon={<GoogleIcon></GoogleIcon>} onClick={handleGoogleLogin}> <MyDivider orientation="vertical" /> Sign in with Google</MyButton>
-    </>
+    <div>
+      <MyButton startIcon={<GoogleIcon/>} onClick={handleGoogleLogin}> <MyDivider orientation="vertical" /> 구글 계정 로그인</MyButton>
+      <MyButton startIcon={<AccessibilityIcon/>} onClick={handleLocalLogin}> <MyDivider orientation="vertical" /> 비회원 로그인</MyButton>
+    </div>
   );
 }
 
